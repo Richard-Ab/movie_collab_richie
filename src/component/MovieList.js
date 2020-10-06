c82440f22cc9366c504d96ff74cc92a218ad191b
unknown <richieb_1234@yahoo.com>
2020-10-06 15:40:14 -0500

    Added a movielist component and also some static movie name

---
+++ src/component/MovieList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function MovieList() {
+    return (
+        <div>
+        <h1>Movie List</h1>
+        <li>dbz</li>
+        <li>harry</li>
+        <li>warlord</li>
+        <li>dooms</li>
+        <li>crazy</li>
+        </div>
+    )
+}
+
+export default MovieList
--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import MovieList from '../component/MovieList'
 
 function HomePage() {
   return (
     <div>
       <h1 className="heading">Movies R Us</h1>
+      <MovieList/>
     </div>
   )
 }
