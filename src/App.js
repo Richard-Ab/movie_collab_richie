0630009efdab1acfef57b39d9921fc066c83218c
unknown <richieb_1234@yahoo.com>
2020-10-07 16:35:01 -0500

    Navigation bar was added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Nav from './component/Nav';
 import HomePage from './page/HomePage'
 
 function App() {
   return (
     <div className="App">
+    <Nav/>
      <HomePage/>
     </div>
   );
--- src/page/HomePage.js
+++ src/page/HomePage.js
@@ -1,19 +1,17 @@
 import React from 'react'
 import MovieList from '../component/MovieList'
 
-
-
-
-
-
-
-
 const HomePage = () => {
-
- 
   return (
     <div>
-      <h1 className="heading">Movies R Us</h1>
+  
+      <h1>Trending</h1>
+      <MovieList/>
+      <h1>Top Rated</h1>
+      <MovieList />
+      <h1>Drama</h1>
+      <MovieList />
+      <h1>Action</h1>
       <MovieList/>
     </div>
   )
